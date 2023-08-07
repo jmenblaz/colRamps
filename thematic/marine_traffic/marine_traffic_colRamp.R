912018049d6a1275cce22abc4d55d69f8c531a68
jmenblaz <javier.menendezruv.es>
2023-08-07 19:35:11 +0200

    updated

---
+++ thematic/marine_traffic/marine_traffic_colRamp.R
@@ -0,0 +1,19 @@
+# colRamps
+#-------------------------------------------------------------------------------
+
+# Marine traffic colour palette
+
+#-------------------------------------------------------------------------------
+# GitHub: jmenblaz | Javier Menéndez-Blázquez
+#-------------------------------------------------------------------------------
+
+library(RColorBrewer)
+
+# 1) Vessel density
+colRamp <- colorRampPalette(c('#6CA6CD','#B0E2FF','#FFFACD','#FFEC8B','#FFA07A','#8B3626', '#130705'))(50)
+
+# 2) Vessel density
+colRamp <- colorRampPalette(c('#FFFAFA','#FFE4E1','#FFC1C1','#FF8C69','#CD5B45','#8B3E2F', '#2B130E'))(50)
+
+# 3) Vessel navigation time
+colRamp <- colorRampPalette(c('#B0E2FF','#FFEC8B','#FFA07A','#8B3626','#130705'))(50)
