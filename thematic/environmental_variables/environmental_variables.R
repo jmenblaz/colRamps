2df44bc568811262935e2d630d4d18c5a1e33165
jmenblaz <javier.menendezruv.es>
2023-08-26 10:22:17 +0200

    Updated

---
+++ thematic/environmental_variables/environmental_variables.R
@@ -0,0 +1,15 @@
+
+# colRamps
+#-------------------------------------------------------------------------------
+
+# Environmental variables
+
+#-------------------------------------------------------------------------------
+# GitHub: jmenblaz | Javier Menéndez-Blázquez
+#-------------------------------------------------------------------------------
+
+library(RColorBrewer)
+
+
+# Sea surface primary swell
+colRamp <- colorRampPalette(c('#ecf9ff','#BFEFFF','#97C8EB','#4682B4','#264e76','#162e46'))(50)
