c76958e19d9d995d3fbd2112a2833b6dc3e9baab
jmenblaz <javier.menendezruv.es>
2023-08-29 17:50:09 +0200

    Updated

--- thematic/environmental_variables/environmental_variables.R
+++ thematic/environmental_variables/environmental_variables.R
@@ -13,3 +13,6 @@ library(RColorBrewer)
 
 # Sea surface primary swell
 colRamp <- colorRampPalette(c('#ecf9ff','#BFEFFF','#97C8EB','#4682B4','#264e76','#162e46'))(50)
+
+# Wind
+colRamp <- colorRampPalette(c('#DAE9D1','#C7E9AC','#AEE184','#99D55A','#74A244','#1E2911'))(50)
