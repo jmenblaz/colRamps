8434363b8d637091a3c432d1dfb445dc7f5cbc1e
jmenblaz <javier.menendezruv.es>
2023-10-16 15:58:37 +0200

    Updated

--- thematic/environmental_variables/environmental_variables.R
+++ thematic/environmental_variables/environmental_variables.R
@@ -5,14 +5,14 @@
 # Environmental variables
 
 #-------------------------------------------------------------------------------
-# GitHub: jmenblaz | Javier Menéndez-Blázquez
+# GitHub: @jmenblaz | Javier Menéndez-Blázquez
 #-------------------------------------------------------------------------------
 
 library(RColorBrewer)
 
 
-# Sea surface primary swell
+# Blue gradient: Sea surface primary swell, bathymetry
 colRamp <- colorRampPalette(c('#ecf9ff','#BFEFFF','#97C8EB','#4682B4','#264e76','#162e46'))(50)
 
-# Wind
+# Gren gradient: Wind
 colRamp <- colorRampPalette(c('#DAE9D1','#C7E9AC','#AEE184','#99D55A','#74A244','#1E2911'))(50)
--- thematic/marine_traffic/marine_traffic_colRamp.R
+++ thematic/marine_traffic/marine_traffic_colRamp.R
@@ -13,7 +13,7 @@ library(RColorBrewer)
 # 1) Vessel density
 colRamp <- colorRampPalette(c('#6CA6CD','#B0E2FF','#FFFACD','#FFEC8B','#FFA07A','#8B3626', '#130705'))(50)
 
-# 2) Vessel density
+# 2) Variation Index of Vessel density
 colRamp <- colorRampPalette(c('#FFFAFA','#FFE4E1','#FFC1C1','#FF8C69','#CD5B45','#8B3E2F', '#2B130E'))(50)
 
 # 3) Vessel navigation time
